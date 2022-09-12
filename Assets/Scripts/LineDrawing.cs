d5c4d4f40dfe26b36b392d158fc64cfd9cbd65b3
abner <abnermtj@live.com.sg>
2022-09-12 23:46:02 +0800

    Update straight line formula

--- Assets/Scripts/LineDrawing.cs
+++ Assets/Scripts/LineDrawing.cs
@@ -40,12 +40,12 @@ public class LineDrawing : DrawManager
 
     public static void LinearRegression(
         List<Vector2> vals,
+        bool flip,
         out double rSquared,
         out double yIntercept,
         out double slope)
     {
         var count = vals.Count;
-        Debug.Log(count);
         double sumOfX = 0;
         double sumOfY = 0;
         double sumOfXSq = 0;
@@ -54,8 +54,9 @@ public class LineDrawing : DrawManager
 
         for (var i = 0; i < count; i++)
         {
-            double x = vals[i].x;
-            double y = vals[i].y;
+            double x = flip ? vals[i].y : vals[i].x;
+            double y = flip ? vals[i].x : vals[i].y;
+
             sumCodeviates += x * y;
             sumOfX += x;
             sumOfY += y;
@@ -71,13 +72,56 @@ public class LineDrawing : DrawManager
 
         double meanX = sumOfX / count;
         double meanY = sumOfY / count;
-        Debug.Log(rNumerator);
-        Debug.Log(rDenom);
         double dblR = rNumerator / Mathf.Sqrt((float)rDenom);
 
         rSquared = dblR * dblR;
         yIntercept = meanY - ((sCo / ssX) * meanX);
         slope = sCo / ssX;
+    }
+
+    // ax+by+c=0
+    // This is a positive value
+    private float distPosToLine(float a, float b, float c, Vector2 pos)
+    {
+        float numer = Mathf.Abs(a * pos.x + b * pos.y + c);
+        float denom = Mathf.Sqrt(a * a + b * b);
+        return numer / denom;
+    }
+
+    // 1.0f points for really straight lines
+    // 0 points for poor lines
+    private float MeasureStraightness2(List<Vector2> points)
+    {
+        // https://brilliant.org/wiki/dot-product-distance-between-point-and-a-line/
+
+        // Create a line between end points
+        // ax+by+c=0
+        Vector2 startPos = points[0];
+        Vector2 endPos = points[points.Count - 1];
+        //A=y1-y2
+        float a = startPos.y - endPos.y;
+
+        //B = x2 - x1
+        float b = endPos.x - startPos.x;
+
+        //C = x1 * y2 - y1 * x2
+        float c = startPos.x * endPos.y - startPos.y * endPos.x;
+
+        int count = points.Count;
+        float sumError = 0;
+        for (int i = 1; i < count - 1; i++)
+        {
+            sumError += distPosToLine(a, b, c, points[i]);
+        }
+
+        //Debug.Log(sumError);
+        //Debug.Log(count);
+        float avgError = sumError / count;
+
+        float score = Mathf.Clamp((100.0f - avgError) / 100.0f, 0.0f, 1.0f);
+
+        Debug.Log(avgError);
+        return score;
     }
 
     //// Measures the straightness of a line from 0 to 1. With 1 being the most straight.
@@ -113,7 +157,14 @@ public class LineDrawing : DrawManager
     private float MeasureStraightness(List<Vector2> points)
     {
         double rSquared, intercept, slope;
-        LinearRegression(points, out rSquared, out intercept, out slope);
+        LinearRegression(points, flip: false, out rSquared, out intercept, out slope);
+
+        // If the points are vertical then need to flip x,y because X is the DV now
+        if (Mathf.Abs((float)slope) > 2)
+        {
+            Debug.Log("FLIP");
+            LinearRegression(points, flip: true, out rSquared, out intercept, out slope);
+        }
 
         foreach (Vector2 point in points)
             Debug.Log(point);
@@ -146,7 +197,7 @@ public class LineDrawing : DrawManager
 
         if (pointerJustReleased)
         {
-            straightness = MeasureStraightness(strokePoints);
+            straightness = MeasureStraightness2(strokePoints);
             straightnessLabel.GetComponent<Text>().text = String.Format("{0:P2}", straightness);
             strokePoints = new List<Vector2>();
         }
@@ -157,14 +208,14 @@ public class LineDrawing : DrawManager
             //Debug.Log($"Straightness {straightness}");
             if (numTargets == 2)
             {
-                straightness = MeasureStraightness(strokePoints);
+                straightness = MeasureStraightness2(strokePoints);
                 if (float.IsNaN(straightness))
                 {
                     return;
                 }
                 if (straightness < straightnessGoal)
                 {
-                    Debug.Log("Not straight enough");
+                    Debug.Log("Not straight enough Straightness: " + straightness);
                     GameObject DamageText = Instantiate(comboPrefab, canvas.transform);
                     DamageText.GetComponent<RectTransform>().anchorMin = new Vector2(0, 0);
                     DamageText.GetComponent<RectTransform>().anchorMax = new Vector2(0, 0);
